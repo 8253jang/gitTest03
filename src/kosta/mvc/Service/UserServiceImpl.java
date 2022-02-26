2db31b833a73c6056353ea6c364990568a3110eb
ChanBum <rew9732@gmail.com>
2022-02-27 01:53:49 +0900

    Bum UserServiceImpl 추가

---
+++ src/kosta/mvc/Service/UserServiceImpl.java
@@ -0,0 +1,17 @@
+package kosta.mvc.Service;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public void insert() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void select() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
